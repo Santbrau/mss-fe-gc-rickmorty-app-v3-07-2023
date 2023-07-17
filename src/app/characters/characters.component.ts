1abda67c62b2314b2274f9cd4508f8c47455a031
Santbrau <marcosantoroserrano@gmail.com>
2023-07-17 19:00:52 +0200

    arreglos

--- src/app/characters/characters.component.ts
+++ src/app/characters/characters.component.ts
@@ -8,58 +8,74 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./characters.component.css']
 })
 export class CharactersComponent implements OnInit {
-  characters: any[] = [];
-  displayedCharacters: any[] = [];
-  selectedCharacter: any = null;
-  newCharacter: any = {};
-  searchQuery: string = '';
+  characters: any[] = []; // Arreglo para almacenar los personajes de la API
+  displayedCharacters: any[] = []; // Arreglo para almacenar los personajes mostrados en pantalla
+  selectedCharacter: any = null; // Variable para almacenar el personaje seleccionado
+  newCharacter: any = {}; // Nuevo personaje a agregar desde el formulario
+  searchQuery: string = ''; // Consulta de búsqueda del formulario
+  createdCharacters: any[] = []; // Arreglo para almacenar los personajes creados por el formulario
 
   constructor(private http: HttpClient, private route: ActivatedRoute) {}
 
   ngOnInit() {
     this.route.queryParams.subscribe(params => {
-      this.searchQuery = params['search'] || '';
-      this.searchCharacters();
+      this.searchQuery = params['search'] || ''; // Obtener la consulta de búsqueda de los parámetros de la URL
+      this.getCharacters(); // Obtener los personajes de la API y aplicar la búsqueda
     });
-    this.getCharacters();
   }
 
   getCharacters() {
-    this.http.get<any>('http://localhost:3000/api/characters').subscribe(data => {
-      this.characters = data;
-      this.searchCharacters();
+    this.http.get<any>('https://rickandmortyapi.com/api/character').subscribe(data => {
+      this.characters = data.results; // Almacenar los personajes de la API en el arreglo
+      this.searchCharacters(); // Aplicar el filtro de búsqueda
     });
   }
 
   searchCharacters() {
-    if (this.searchQuery.trim() !== '') {
-      this.displayedCharacters = this.characters.filter(character => {
-        return character.name.toLowerCase().includes(this.searchQuery.toLowerCase());
+    const allCharacters = this.characters.concat(this.createdCharacters); // Combinar los personajes de la API y los creados por el formulario
+
+    if (this.searchQuery.trim() !== '') { // Verificar si hay una consulta de búsqueda ingresada
+      this.displayedCharacters = allCharacters.filter(character => {
+        return character.name.toLowerCase().includes(this.searchQuery.toLowerCase()); // Filtrar los personajes según la consulta de búsqueda
       });
     } else {
-      this.displayedCharacters = this.characters;
+      this.displayedCharacters = this.getRandomCharacters(4); // Mostrar personajes aleatorios si no hay consulta de búsqueda
     }
   }
 
+  getRandomCharacters(count: number): any[] {
+    const totalCharacters = this.characters.length;
+    const randomCharacters: any[] = [];
+    const availableIndices = [...Array(totalCharacters).keys()]; // Crear un arreglo con los índices disponibles
+
+    while (randomCharacters.length < count && availableIndices.length > 0) {
+      const randomIndex = Math.floor(Math.random() * availableIndices.length);
+      const characterIndex = availableIndices[randomIndex];
+      const randomCharacter = this.characters[characterIndex];
+
+      randomCharacters.push(randomCharacter);
+      availableIndices.splice(randomIndex, 1); // Eliminar el índice seleccionado de los disponibles
+    }
+
+    return randomCharacters;
+  }
+
   showCharacterDetails(character: any) {
-    this.selectedCharacter = character;
+    this.selectedCharacter = character; // Asignar el personaje seleccionado
   }
 
   closeCharacterDetails() {
-    this.selectedCharacter = null;
+    this.selectedCharacter = null; // Cerrar el panel de detalles del personaje
   }
 
   addCharacter() {
-    this.http.post<any>('http://localhost:3000/api/characters', this.newCharacter).subscribe(data => {
-      this.characters.push(data);
-      this.newCharacter = {};
-    });
+    this.createdCharacters.push(this.newCharacter); // Agregar el nuevo personaje desde el formulario al arreglo de personajes creados
+    this.newCharacter = {}; // Limpiar el formulario
+    this.searchCharacters(); // Aplicar el filtro de búsqueda
   }
 
   deleteCharacter(characterId: number) {
-    this.http.delete<any>(`http://localhost:3000/api/characters/${characterId}`).subscribe(() => {
-      this.characters = this.characters.filter(character => character.id !== characterId);
-      this.displayedCharacters = this.displayedCharacters.filter(character => character.id !== characterId);
-    });
+    this.createdCharacters = this.createdCharacters.filter(character => character.id !== characterId); // Eliminar el personaje del arreglo de personajes creados
+    this.searchCharacters(); // Aplicar el filtro de búsqueda
   }
 }
